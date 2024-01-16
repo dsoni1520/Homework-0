f0a072942a362e0149789c0fd1571301da9e1bea
dsoni1520 <112735668+dsoni1520@users.noreply.github.com>
2024-01-16 11:54:37 +0530

    Initial commit

---
+++ answer.py
@@ -0,0 +1,22 @@
+def hello_world():
+    # Write some thing in the following doc string
+    """
+    hey there
+    """
+
+    # Comment the following line
+    # Comment this line
+
+    # Print "Hello, World"
+    print("Hello, World")
+hello_world()
+
+def add(a,b):
+
+    # Write simple code to add variable a and b
+    output= a + b
+
+    return output
+
+sum = add(2,3)
+print(sum)
